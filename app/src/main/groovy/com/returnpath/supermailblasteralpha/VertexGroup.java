3f04a7190c4ef1f109e5e5811d0b3c546046eeb1
Chris Butler <chris.butler@returnpath.com>
2015-07-31 13:00:04 -0600

    VertexGroups now keep track of their center
        - Black background.
        - No rotating. We won't use it and it complicates things slightly.

--- app/src/main/groovy/com/returnpath/supermailblasteralpha/VertexGroup.java
+++ app/src/main/groovy/com/returnpath/supermailblasteralpha/VertexGroup.java
@@ -18,7 +18,11 @@ public class VertexGroup {
     // Since we store 3 floats for each vertex, our stride is 3 times the sie of each float.
     final private int mStrideBytes = 3 * 4;
     private int mColorHandle;
+
+    private float[] mVerts;
     private FloatBuffer mBuf;
+    private float mCenterX = 0.0f;
+    private float mCenterY = 0.0f;
 
     private float[] mModelMatrix = new float[16];
 
@@ -29,15 +33,11 @@ public class VertexGroup {
     private float b = 0.5f;
 
     // This constructor expects an array of 'packed' data. It should be
-    // an array where the first 7 indices correspond to x, y, z, r, g, b, and a for the first vertex. The next 7 for the second, etc.
+    // an array where the first 3 indices correspond to x, y, and z for the first vertex. The next 3 for the second, etc.
     public VertexGroup(float[] packed) {
         Matrix.setIdentityM(mModelMatrix, 0);
-        mBuf = ByteBuffer.allocateDirect(packed.length * 4)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
-        mBuf.put(packed);
-        mBuf.position(0);
-
+        mVerts = packed.clone();
+        loadBuf(mVerts);
         color(1.0f, 1.0f, 1.0f);
     }
 
@@ -59,6 +59,14 @@ public class VertexGroup {
         return VertexGroup.makeRect(width, height, 0.0f);
     }
 
+    private void loadBuf(float[] packed) {
+        mBuf = ByteBuffer.allocateDirect(packed.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        mBuf.put(packed);
+        mBuf.position(0);
+    }
+
     public VertexGroup color(float r, float g, float b) {
         this.r = r;
         this.g = g;
@@ -67,19 +75,45 @@ public class VertexGroup {
     }
 
     public VertexGroup scale(float factor) {
-        Matrix.scaleM(mModelMatrix, 0, factor, factor, factor);
+        for (int i = 0; 3 * i < mVerts.length; i += 1) {
+            mVerts[3 * i + 0] *= factor;
+            mVerts[3 * i + 1] *= factor;
+            // The z coord isn't adjusted here.
+            //mVerts[3*i + 2] *= blah;
+        }
+
+        loadBuf(mVerts);
+
         return this;
     }
 
     public VertexGroup shift(float dx, float dy) {
-        Matrix.translateM(mModelMatrix, 0, dx, dy, 0.0f);
+        mCenterX += dx;
+        mCenterY += dy;
+
+        // Every three data points corresponds to the same vertex.
+        for (int i = 0; 3 * i < mVerts.length; i += 1) {
+            mVerts[3 * i + 0] += dx;
+            mVerts[3 * i + 1] += dy;
+            // The z coord isn't adjusted here.
+            //mVerts[3*i + 2] += dz;
+        }
+
+        loadBuf(mVerts);
+
         return this;
     }
 
-    // Rotate the object. Positive is counter-clockwise.
-    public VertexGroup rotate(float dtheta) {
-        Matrix.rotateM(mModelMatrix, 0, dtheta, 0.0f, 0.0f, 1.0f);
-        return this;
+    public VertexGroup center(float x, float y) {
+        return shift(mCenterX - x, mCenterY - y);
+    }
+
+    public float centerX() {
+        return mCenterX;
+    }
+
+    public float centerY() {
+        return mCenterY;
     }
 
     public void draw(float[] mvpMatrix, int mvpHandle, int posHandle, int colorHandle) {
